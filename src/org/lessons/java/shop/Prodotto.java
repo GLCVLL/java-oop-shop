376d7e076dbffa2538fda59b845dfe95573c4fec
Gioele Iacovella <g.ioele96@outlook.it>
2023-11-13 15:48:15 +0100

    first commit

---
+++ src/org/lessons/java/shop/Prodotto.java
@@ -0,0 +1,25 @@
+package org.lessons.java.shop;
+
+public class Prodotto {
+
+	private int code;
+	private int name;
+	private int text;
+	private int price;
+	private int vat;
+	
+	public Prodotto(int code, int name, int text, int price, int vat) {
+		
+		setCode(code);
+		setName(name);
+		setText(text);
+		setPrice(price);
+		setVat(vat);
+		
+	}
+	
+	public int getCode{
+		return code;
+	}
+	
+}
